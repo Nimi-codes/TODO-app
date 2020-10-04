22cf45f6cdf3cba1c5e8eeb98ca66386de635dbc
Nimisha Mishra <nimishamishrastp@gmail.com>
2020-10-04 18:14:21 +0530

    Second commit:add view engine and connect assets

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 const express=require('express');
 const app=express();
 const port=8000;
+app.use('/',require('./routes'));
+app.set('view engine','ejs');
+app.set('views','./views');
+app.use(express.urlencoded());
+app.use(express.static('assets'));
 app.listen(port,function(err){
     if(err){
         console.log(`There is an an errror while running server: ${err}`);
